74950f559f1d3b5e458b474cd1243593e74311f0
DESKTOP-8UKB9DM\Chinh Do <chinhdnk@gmail.com>
2022-08-16 21:26:12 +0200

    add search filter

--- WebClient/src/app/app.component.ts
+++ WebClient/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct} from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +7,11 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Pi Store';
-  products: IProduct [];
-  constructor(private http: HttpClient) {
-    this.products = [];
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/Products?pageSize=50').subscribe((response: any) => {
-      this.products=response.data;
-    }, error => {
-      console.log(error);
-    });
+    
   }
 }
 
