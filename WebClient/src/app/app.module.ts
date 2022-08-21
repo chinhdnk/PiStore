25b4505843c537812ea6d2cb331262be1ec0ac9b
DESKTOP-8UKB9DM\Chinh Do <chinhdnk@gmail.com>
2022-08-21 15:49:12 +0200

    Decorate UI

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ErrorInterceptor } from './core/error.interceptor';
 import { HomeModule } from './home/home.module';
-import { ShopModule } from './shop/shop.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -15,14 +17,16 @@ import { ShopModule } from './shop/shop.module';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     CoreModule,
-    ShopModule,
-    HomeModule
+    HomeModule,
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi: true}],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- WebClient/src/app/shop/product-detail/product-detail.component.ts
+++ WebClient/src/app/shop/product-detail/product-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IPagination } from '../../shared/models/pagination';
 import { IProduct } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,7 +11,9 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailComponent implements OnInit {
   product!:IProduct 
-  constructor( private shopService:ShopService, private activatedRoute:ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+    this.bcService.set('@productDetails', '');
+  }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -21,6 +23,7 @@ export class ProductDetailComponent implements OnInit {
   loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
     });
